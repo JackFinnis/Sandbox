a6c1f779078a74b1325b848cad00db29faf6aedf
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-08-02 14:42:58 +0100

    Initial Commit

---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by William Finnis on 02/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
